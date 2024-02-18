88fbabc2960e8c6933676104aef19485af6e8ed2
Miguel Mendes <m_n_mendes@hotmail.com>
2024-02-18 18:57:33 +0000

    Modal unit tests

--- src/app/components/share-modal/share-modal.component.spec.ts
+++ src/app/components/share-modal/share-modal.component.spec.ts
@@ -1,23 +1,49 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
 import { ShareModalComponent } from './share-modal.component';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('ShareModalComponent', () => {
+fdescribe('ShareModalComponent', () => {
   let component: ShareModalComponent;
   let fixture: ComponentFixture<ShareModalComponent>;
+  let dialogRefSpy: jasmine.SpyObj<MatDialogRef<ShareModalComponent>>;
+  let snackBarSpy: jasmine.SpyObj<MatSnackBar>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ShareModalComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(waitForAsync(() => {
+    const matDialogRefSpy = jasmine.createSpyObj('MatDialogRef', ['close']);
+    const matSnackBarSpy = jasmine.createSpyObj('MatSnackBar', ['open']);
+
+    TestBed.configureTestingModule({
+      declarations: [ShareModalComponent],
+      
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: matDialogRefSpy },
+        { provide: MatSnackBar, useValue: matSnackBarSpy },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
+    }).compileComponents();
+
+    dialogRefSpy = TestBed.inject(MatDialogRef) as jasmine.SpyObj<MatDialogRef<ShareModalComponent>>;
+    snackBarSpy = TestBed.inject(MatSnackBar) as jasmine.SpyObj<MatSnackBar>;
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(ShareModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  
+  it('should open Twitter with joke text', () => {
+    const jokeText = 'This is a joke';
+    spyOn(window, 'open');
+    component.tweet(jokeText);
+    const expectedUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(jokeText)}`;
+    expect(window.open).toHaveBeenCalledWith(expectedUrl, '_blank');
   });
+
 });
