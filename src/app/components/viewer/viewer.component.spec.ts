d692081e8cbad68b7c6c63f469169eea9f6bdd2a
Miguel Mendes <m_n_mendes@hotmail.com>
2024-02-18 15:50:21 +0000

    First unit tests for viewer

--- src/app/components/viewer/viewer.component.spec.ts
+++ src/app/components/viewer/viewer.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { ViewerComponent } from './viewer.component';
+import { TopBarComponent } from '../top-bar/top-bar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('ViewerComponent', () => {
+fdescribe('ViewerComponent', () => {
   let component: ViewerComponent;
   let fixture: ComponentFixture<ViewerComponent>;
+  let httpClientSpy: { get: jasmine.Spy };
+  let matDialogSpy: { open: jasmine.Spy };
+  let matSnackBarSpy: { open: jasmine.Spy };
+ 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ViewerComponent]
+      declarations: [ViewerComponent, 
+                     TopBarComponent, 
+                     FooterComponent
+                    ],
+                    schemas: [
+                      CUSTOM_ELEMENTS_SCHEMA
+                    ],
+
+      imports: [HttpClientTestingModule, 
+                RouterTestingModule, 
+               ],
+                
+      providers: [{provide: MatDialog, useValue: matDialogSpy},
+                 {provide: MatSnackBar, useValue: matSnackBarSpy},
+      ],
     })
     .compileComponents();
     
@@ -17,7 +42,7 @@ describe('ViewerComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
-  });
+    });
 });
