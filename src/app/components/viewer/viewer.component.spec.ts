533c8bcffbaaa1a44039997f08e003f2e2572c7d
Miguel Mendes <m_n_mendes@hotmail.com>
2024-02-18 18:19:32 +0000

    Code cleanup

--- src/app/components/viewer/viewer.component.spec.ts
+++ src/app/components/viewer/viewer.component.spec.ts
@@ -1,49 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { of, throwError } from 'rxjs';
 import { ViewerComponent } from './viewer.component';
 import { TopBarComponent } from '../top-bar/top-bar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('ViewerComponent', () => {
+fdescribe('ViewerComponent', () => {
   let component: ViewerComponent;
   let fixture: ComponentFixture<ViewerComponent>;
   let httpClientSpy: { get: jasmine.Spy };
   let matDialogSpy: { open: jasmine.Spy };
   let matSnackBarSpy: { open: jasmine.Spy };
  
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [ViewerComponent, 
+                      TopBarComponent, 
+                      FooterComponent
+                      ],
+                      schemas: [
+                        CUSTOM_ELEMENTS_SCHEMA
+                      ],
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ViewerComponent, 
-                     TopBarComponent, 
-                     FooterComponent
-                    ],
-                    schemas: [
-                      CUSTOM_ELEMENTS_SCHEMA
-                    ],
+        imports: [HttpClientTestingModule, 
+                  RouterTestingModule, 
+                ],
+                  
+        providers: [{provide: MatDialog, useValue: matDialogSpy},
+                  {provide: MatSnackBar, useValue: matSnackBarSpy},
+        ],
+      })
+      .compileComponents();
+      
+      fixture = TestBed.createComponent(ViewerComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-      imports: [HttpClientTestingModule, 
-                RouterTestingModule, 
-               ],
-                
-      providers: [{provide: MatDialog, useValue: matDialogSpy},
-                 {provide: MatSnackBar, useValue: matSnackBarSpy},
-      ],
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ViewerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create the component', () => {
-    expect(component).toBeTruthy();
+    it('should create the component', () => {
+      expect(component).toBeTruthy();
     });
 
     it('should call checkForSensitiveContent method and update flags - option death', () => {
@@ -62,7 +61,7 @@ describe('ViewerComponent', () => {
       expect(component.deathJoke).toBeFalse();
       expect(component.isSensitive).toBeTruthy();
       expect(component.chuckKicked).toBeFalse();
-      });
+    });
 
     it('should call checkForSensitiveContent method and update flags - option sensitive', () => {
       const jokeValue = 'This is a kick joke';
@@ -71,13 +70,37 @@ describe('ViewerComponent', () => {
       expect(component.deathJoke).toBeFalse();
       expect(component.isSensitive).toBeFalse();
       expect(component.chuckKicked).toBeTrue();
-      });
+    });
         
-      it('should call checkForSensitiveContent method and update flags - not specific case', () => {
+    it('should call checkForSensitiveContent method and update flags - not specific case', () => {
       const jokeValue = 'This is a random joke';
       component.checkForSensitiveContent(jokeValue);
       expect(component.deathJoke).toBeFalse();
       expect(component.isSensitive).toBeFalse();
       expect(component.chuckKicked).toBeFalse();
-      });
+    });
+
+    it('should call getJoke method - error', () => {
+      const spyGet = spyOn(component['http'], 'get').and.returnValue(throwError(() => new Error('error')))
+      const spyCheckSensitiveContent = spyOn(component, 'checkForSensitiveContent').and.stub();
+      const timesChuckedBefore = component.timesChucked;
+        
+      component.getJoke();
+      expect(component.timesChucked).toEqual(timesChuckedBefore)
+      expect(spyGet).toHaveBeenCalled();
+      expect(spyCheckSensitiveContent).not.toHaveBeenCalled();
+    });
+
+    it('should call getJoke method - success', fakeAsync(()=> {
+      const spyGet = spyOn(component['http'], 'get').and.returnValue(of({value: 'my joke'}))
+      const spyCheckSensitiveContent = spyOn(component, 'checkForSensitiveContent').and.stub();
+          
+      component.getJoke();
+      tick();
+          
+      expect(spyGet).toHaveBeenCalled();
+      expect(spyCheckSensitiveContent).toHaveBeenCalledWith('my joke');
+      expect(component.jokeValue).toEqual('my joke');
+    }));
+
 });
